f79544b5fe83dd5a5854248bdffbecafe0e6f9b9
Radoslaw <radekbolin@gmail.com>
2022-01-07 17:02:15 +0100

    Prototyp rysunku szubienicy

---
+++ szubienica.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+int main()
+{
+    //SZUBIENICA PROTOTYP
+    int bledy;
+
+    scanf("%d", &bledy);
+    getchar();
+    if(bledy==1)
+    {
+    printf("__________\n");
+    printf("|        |\n");
+    printf("¯¯¯¯¯¯¯¯¯¯");
+    }
+
+    if(bledy==2)
+{
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("__________\n");
+    printf("|        |\n");
+    printf("¯¯¯¯¯¯¯¯¯¯");
+}
+    if(bledy==3)
+    {
+    printf("      ______\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("__________\n");
+    printf("|        |\n");
+    printf("¯¯¯¯¯¯¯¯¯¯");
+
+    }
+        if(bledy==4)
+    {
+    printf("      ______\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |    (¯)\n");
+    printf("     |     \n");
+    printf("     |     \n");
+    printf("__________\n");
+    printf("|        |\n");
+    printf("¯¯¯¯¯¯¯¯¯¯");
+
+    }
+        if(bledy==5)
+    {
+    printf("      ______\n");
+    printf("     |     |\n");
+    printf("     |     |\n");
+    printf("     |    (¯)\n");
+    printf("     |     |\n");
+    printf("     |    / \\ \n");
+    printf("__________\n");
+    printf("|        |\n");
+    printf("¯¯¯¯¯¯¯¯¯¯");
+
+    }
+}
